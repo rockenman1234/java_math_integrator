3b5cda456501cf7114dc940b4d1c397ed1f3781a
Alex Jenkins <22626564+rockenman1234@users.noreply.github.com>
2022-08-28 20:45:13 +0000

    Update Postive_Iteration.java

--- Postive_Iteration.java
+++ Postive_Iteration.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Iteration {
+public class Positve_Iteration {
    public static void main(String[] args){
    Scanner stdin = new Scanner(System.in);
    //Starts Method and starts scanner
